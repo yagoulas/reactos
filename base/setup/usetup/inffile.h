9806cc1b519544e89997367b5ba1ead675a429de
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2017-07-14 22:33:15 +0000

    Sync with trunk r75344 to get the inflib stuff in place.
    svn path=/branches/setup_improvements/; revision=75345

--- base/setup/usetup/inffile.h
+++ base/setup/usetup/inffile.h
@@ -74,10 +74,12 @@ extern BOOLEAN InfGetStringField(PINFCONTEXT Context,
  * Delete it once we don't use inflib anymore */
 typedef struct _INFCONTEXT
 {
-    PVOID Inf;
-    PVOID Section;
-    PVOID Line;
+  PVOID Inf;
+  PVOID CurrentInf;
+  PVOID Section;
+  PVOID Line;
 } INFCONTEXT;
+C_ASSERT(sizeof(INFCONTEXT) == 2 * sizeof(PVOID) + 2 * sizeof(UINT));
 
 BOOL
 WINAPI
