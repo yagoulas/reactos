f5491c4f41f3e826610a3aa84364456f1301c416
Giannis Adamopoulos <gadamopoulos@reactos.org>
2017-11-27 00:21:00 +0200

    [ROSLAUNCHER] -Initial gui implementation

---
+++ base/applications/roslauncher/resource.h
@@ -0,0 +1,25 @@
+
+#define IDC_STATIC1 1
+#define IDC_STATIC2 2
+#define IDC_STATIC3 3
+#define IDC_STATIC4 4
+#define IDC_STATIC5 5
+#define IDD_LAUNCHER_MAIN               106
+#define IDD_LAUNCHER_CHANNELS           107
+#define IDD_LAUNCHERS_ENVIRONMENT       108
+#define IDC_EDIT1                       1002
+#define IDC_BUTTON1                     1003
+#define IDC_EDIT2                       1004
+#define IDC_RADIO1                      1005
+#define IDC_RADIO2                      1006
+#define IDC_RADIO3                      1007
+#define IDC_EDIT3                       1008
+#define IDC_CHECK1                      1009
+#define IDC_CHECK2                      1010
+#define IDC_CHECK3                      1011
+#define IDC_EDIT4                       1012
+#define IDC_EDIT5                       1013
+#define IDC_COMBO1                      1018
+#define IDC_BUTTON2                     1019
+#define IDC_BUTTON3                     1020
+#define IDC_LIST1                       1021
--- base/applications/roslauncher/roslauncher.cpp
+++ base/applications/roslauncher/roslauncher.cpp
@@ -1,6 +1,81 @@
 #include <windows.h>
+#include <commctrl.h>
+#include <atlbase.h>
+#include <atlcom.h>
+#include <atlwin.h>
+#include <rosdlgs.h>
+#include "resource.h"
+
+class CLauncherModule : public CComModule
+{
+public:
+};
+
+BEGIN_OBJECT_MAP(ObjectMap)
+END_OBJECT_MAP()
+
+CLauncherModule gModule;
+CAtlWinModule gWinModule;
+
+class CMainPage : public CPropertyPageImpl<CMainPage>
+{
+public: 
+    enum { IDD = IDD_LAUNCHER_MAIN };
+
+};
+
+class CEnvironmentPage : public CPropertyPageImpl<CEnvironmentPage>
+{
+public: 
+    enum { IDD = IDD_LAUNCHERS_ENVIRONMENT };
+
+};
+
+class CChannelsPage : public CPropertyPageImpl<CChannelsPage>
+{
+public: 
+    enum { IDD = IDD_LAUNCHER_CHANNELS };
+
+};
+
+int CALLBACK PropSheetProc(HWND hwndDlg, UINT uMsg, LPARAM lParam)
+{
+    if (uMsg == PSCB_INITIALIZED)
+    {
+        SetDlgItemText(hwndDlg, IDOK, L"Launch");
+    }
+
+    return 0;
+}
 
 INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, INT nShowCmd)
 {
+    gModule.Init(ObjectMap, hInstance, NULL);
+    InitCommonControls();
+
+    PROPSHEETHEADER psh;
+    HPROPSHEETPAGE hpsp[3];
+    CMainPage mainPage;
+    CEnvironmentPage envPage;
+    CChannelsPage channelsPage;
+
+    hpsp[0] = mainPage.Create();
+    hpsp[1] = envPage.Create();
+    hpsp[2] = channelsPage.Create();
+
+    ZeroMemory(&psh, sizeof(psh));
+    psh.dwSize = sizeof(psh);
+    psh.dwFlags = PSH_NOAPPLYNOW | PSH_USECALLBACK;
+    psh.pfnCallback = PropSheetProc;
+    psh.hwndParent = NULL;
+    psh.hInstance = hInstance;
+    psh.hIcon = NULL;
+    psh.pszCaption = L"Reactos logger";
+    psh.nPages = _countof(hpsp);
+    psh.nStartPage = 0;
+    psh.phpage = hpsp;
+
+    PropertySheet(&psh);
+
     return 0;
 }
