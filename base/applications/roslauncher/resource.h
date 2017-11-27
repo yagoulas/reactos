ce87a120f584b0d6a88815a7cc35e1b072bc0e10
Giannis Adamopoulos <gadamopoulos@reactos.org>
2017-11-27 12:57:47 +0200

    [ROSLAUNCHER] Implement the browse button and rename the resource constants

--- base/applications/roslauncher/resource.h
+++ base/applications/roslauncher/resource.h
@@ -1,27 +1,33 @@
 
-#define IDC_STATIC1 1
-#define IDC_STATIC2 2
-#define IDC_STATIC3 3
-#define IDC_STATIC4 4
-#define IDC_STATIC5 5
-#define IDC_STATIC6 6
+#define IDC_STATIC1 201
+#define IDC_STATIC2 202
+#define IDC_STATIC3 203
+#define IDC_STATIC4 204
+#define IDC_STATIC5 205
+#define IDC_STATIC6 206
+
 #define IDD_LAUNCHER_MAIN               106
 #define IDD_LAUNCHER_CHANNELS           107
 #define IDD_LAUNCHERS_ENVIRONMENT       108
-#define IDC_EDIT1                       1002
-#define IDC_EDIT2                       1004
-#define IDC_EDIT3                       1008
-#define IDC_EDIT4                       1012
-#define IDC_EDIT5                       1013
-#define IDC_EDIT6                       1014
-#define IDC_RADIO1                      1005
-#define IDC_RADIO2                      1006
-#define IDC_RADIO3                      1007
-#define IDC_CHECK1                      1009
-#define IDC_CHECK2                      1010
-#define IDC_CHECK3                      1011
-#define IDC_COMBO1                      1018
-#define IDC_BUTTON1                     1003
-#define IDC_BUTTON2                     1019
-#define IDC_BUTTON3                     1020
-#define IDC_LIST1                       1021
+
+#define IDC_EXECUTABLE  1001
+#define IDC_PARAMS      1002
+#define IDC_LOGFILE     1003
+#define IDC_STARTDIR    1004
+#define IDC_USERNAME    1005
+#define IDC_PASSWRD     1006
+#define IDC_LOGTOCON    1008
+#define IDC_LOGTOFILE   1009
+#define IDC_LOGTOBOTH   1010
+#define IDC_DPH         1011
+#define IDC_LDRSNAPS    1012
+#define IDC_RUNAS       1013
+#define IDC_BROWSE      1014
+
+#define IDC_ADD         1020
+#define IDC_EDIT        1021
+#define IDC_REMOVE      1022
+#define IDC_NEWCHNL     1023
+#define IDC_NEWVAR      1024
+#define IDC_ENVLIST     1025
+#define IDC_CHNLLIST    1026
--- base/applications/roslauncher/roslauncher.cpp
+++ base/applications/roslauncher/roslauncher.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <commctrl.h>
+#include <commdlg.h>
 #include <atlbase.h>
 #include <atlcom.h>
 #include <atlwin.h>
@@ -37,44 +38,65 @@ public:
 
     BEGIN_MSG_MAP(CMainPage)
         MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+        COMMAND_ID_HANDLER(IDC_BROWSE, OnBrowse)
         CHAIN_MSG_MAP(CPropertyPageImpl<CMainPage>)
     END_MSG_MAP()
 
+    LRESULT OnBrowse(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL &bHandled)
+    {
+        OPENFILENAMEW ofn = {sizeof(ofn)};
+        CStringW strFileName;
+
+        ofn.hwndOwner = m_hWnd;
+        ofn.hInstance = _AtlBaseModule.GetModuleInstance();
+        ofn.lpstrFilter = L"Exe files (*.exe)\0*.exe\0All Files\0*.*\0\0";
+        ofn.nMaxFile = MAX_PATH;
+        ofn.lpstrFile = strFileName.GetBuffer(ofn.nMaxFile);
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_FILEMUSTEXIST;
+
+        strFileName.ReleaseBuffer();
+
+        if (GetOpenFileNameW(&ofn))
+            SetDlgItemText(IDC_EXECUTABLE, strFileName);
+
+        return 0;
+    }
+
     LRESULT OnInitDialog(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled)
     {
-        SetDlgItemText(IDC_EDIT1, m_settings->strExecutable);
-        SetDlgItemText(IDC_EDIT2, m_settings->strParams);
-        SetDlgItemText(IDC_EDIT3, m_settings->strLogFileName);
-        SetDlgItemText(IDC_EDIT4, m_settings->strStartDir);
+        SetDlgItemText(IDC_EXECUTABLE, m_settings->strExecutable);
+        SetDlgItemText(IDC_PARAMS, m_settings->strParams);
+        SetDlgItemText(IDC_LOGFILE, m_settings->strLogFileName);
+        SetDlgItemText(IDC_STARTDIR, m_settings->strStartDir);
         if (m_settings->bLogToFile == true && m_settings->bLogToConsole  == true)
-            CheckDlgButton(IDC_RADIO3, BST_CHECKED);
+            CheckDlgButton(IDC_LOGTOBOTH, BST_CHECKED);
         else if (m_settings->bLogToFile == true)
-            CheckDlgButton(IDC_RADIO2, BST_CHECKED);
+            CheckDlgButton(IDC_LOGTOFILE, BST_CHECKED);
         else if (m_settings->bLogToConsole == true)
-            CheckDlgButton(IDC_RADIO1, BST_CHECKED);
+            CheckDlgButton(IDC_LOGTOCON, BST_CHECKED);
 
         return TRUE;
     }
 
     int OnApply()
     {
-        GetDlgItemText(IDC_EDIT1, m_settings->strExecutable.GetBuffer(MAX_PATH), MAX_PATH);
+        GetDlgItemText(IDC_EXECUTABLE, m_settings->strExecutable.GetBuffer(MAX_PATH), MAX_PATH);
         m_settings->strExecutable.ReleaseBuffer();
-        GetDlgItemText(IDC_EDIT2, m_settings->strParams.GetBuffer(MAX_PATH), MAX_PATH);
+        GetDlgItemText(IDC_PARAMS, m_settings->strParams.GetBuffer(MAX_PATH), MAX_PATH);
         m_settings->strParams.ReleaseBuffer();
-        GetDlgItemText(IDC_EDIT3, m_settings->strLogFileName.GetBuffer(MAX_PATH), MAX_PATH);
+        GetDlgItemText(IDC_LOGFILE, m_settings->strLogFileName.GetBuffer(MAX_PATH), MAX_PATH);
         m_settings->strLogFileName.ReleaseBuffer();
-        GetDlgItemText(IDC_EDIT4, m_settings->strStartDir.GetBuffer(MAX_PATH), MAX_PATH);
+        GetDlgItemText(IDC_STARTDIR, m_settings->strStartDir.GetBuffer(MAX_PATH), MAX_PATH);
         m_settings->strStartDir.ReleaseBuffer();
         
-        if (IsDlgButtonChecked(IDC_RADIO3))
+        if (IsDlgButtonChecked(IDC_LOGTOBOTH))
         {
             m_settings->bLogToFile = m_settings->bLogToConsole = true;
         }
         else
         {
-            m_settings->bLogToFile = IsDlgButtonChecked(IDC_RADIO2);
-            m_settings->bLogToConsole = IsDlgButtonChecked(IDC_RADIO1);
+            m_settings->bLogToFile = IsDlgButtonChecked(IDC_LOGTOFILE);
+            m_settings->bLogToConsole = IsDlgButtonChecked(IDC_LOGTOCON);
         }
 
         return PSNRET_NOERROR;
@@ -125,31 +147,24 @@ int CALLBACK PropSheetProc(HWND hwndDlg, UINT uMsg, LPARAM lParam)
     return 0;
 }
 
-static void EditLaucherSettings(LaucherSettings* settings)
+static bool EditLaucherSettings(LaucherSettings* settings)
 {
-    PROPSHEETHEADER psh;
-    HPROPSHEETPAGE hpsp[3];
     CMainPage mainPage(settings);
     CEnvironmentPage envPage(settings);
     CChannelsPage channelsPage(settings);
 
+    HPROPSHEETPAGE hpsp[3];
     hpsp[0] = mainPage.Create();
     hpsp[1] = envPage.Create();
     hpsp[2] = channelsPage.Create();
 
-    ZeroMemory(&psh, sizeof(psh));
-    psh.dwSize = sizeof(psh);
-    psh.dwFlags = PSH_NOAPPLYNOW | PSH_USECALLBACK;
+    PROPSHEETHEADER psh = {sizeof(psh), PSH_NOAPPLYNOW | PSH_USECALLBACK};
     psh.pfnCallback = PropSheetProc;
-    psh.hwndParent = NULL;
-    psh.hInstance = NULL;
-    psh.hIcon = NULL;
     psh.pszCaption = L"Reactos logger";
     psh.nPages = _countof(hpsp);
-    psh.nStartPage = 0;
     psh.phpage = hpsp;
 
-    PropertySheet(&psh);
+    return (PropertySheet(&psh) >= 1);
 }
 
 static void Launch(LaucherSettings& settings)
@@ -195,7 +210,8 @@ INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     settings.strLogFileName = L"c:\\log.txt";
     settings.bLogToFile = settings.bLogToConsole = true;
     
-    EditLaucherSettings(&settings);
+    if (!EditLaucherSettings(&settings))
+        return 0;
 
     Launch(settings);
     
